e3126e977412b762061bdd9e809fa74033f9af91
Inesh Dey <ineshdey@hotmail.com>
2023-09-05 06:46:52 +0000

    projects

--- api/static/js/3dshenanigans.js
+++ api/static/js/3dshenanigans.js
@@ -1,5 +1,11 @@
 import { Application } from '@splinetool/runtime';
 
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
+const blender = document.getElementById('blender');
+const app = new Application(blender);
 app.load('https://prod.spline.design/8kC6LukeLPjvsmAy/scene.splinecode');
+
+
+
+const FlowMaster = document.getElementById('FlowMaster');
+const app1 = new Application(FlowMaster);
+app1.load('https://prod.spline.design/SpkJeHkl37v2AY-9/scene.splinecode');
