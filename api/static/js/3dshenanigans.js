65bcc9b2097ec9fa0fd2d9e36a22e9a10b4f2fb9
Inesh <79165198+somebody4545@users.noreply.github.com>
2023-09-22 23:04:21 -0700

    Revert "projects"
    This reverts commit e3126e977412b762061bdd9e809fa74033f9af91.

--- api/static/js/3dshenanigans.js
+++ api/static/js/3dshenanigans.js
@@ -1,11 +1,5 @@
 import { Application } from '@splinetool/runtime';
 
-const blender = document.getElementById('blender');
-const app = new Application(blender);
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
 app.load('https://prod.spline.design/8kC6LukeLPjvsmAy/scene.splinecode');
-
-
-
-const FlowMaster = document.getElementById('FlowMaster');
-const app1 = new Application(FlowMaster);
-app1.load('https://prod.spline.design/SpkJeHkl37v2AY-9/scene.splinecode');
