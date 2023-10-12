b8c3b351731a6bac23313355abf54ca4310417e7
Inesh Dey <ineshdey@hotmail.com>
2023-10-11 21:37:27 -0700

    increase wait, most of the page is still too laggy

--- api/static/js/load.js
+++ api/static/js/load.js
@@ -12,7 +12,7 @@ setVisible('.page', false);
 setVisible('#loading', true);
 
 document.addEventListener('DOMContentLoaded', () =>
-  wait(1000).then(() => {
+  wait(3000).then(() => {
     setVisible('.page', true);
     requestAnimationFrame(function() {
         page.classList.add("visible");
