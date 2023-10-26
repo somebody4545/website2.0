1d9ab7de267ff301ee13a69b7e80d32b25eee00e
Inesh <79165198+somebody4545@users.noreply.github.com>
2023-10-25 17:46:20 -0700

    Fixed parallax

--- api/static/js/parallaxbg.js
+++ api/static/js/parallaxbg.js
@@ -2,11 +2,11 @@
     var body = document.getElementById("page"),
             e = document.documentElement,
             scrollPercent;
-    $(window).unbind("scroll").scroll(function () {
-        scrollPercent = ($(window).scrollTop() / body.scrollHeight)*100;
+    body.onscroll = (event) => {
+        scrollPercent = (body.scrollTop / body.scrollHeight)*100;
         console.log(scrollPercent)
-        console.log($(window).scrollTop())
+        console.log(body.scrollTop)
         console.log(body.scrollHeight)
         body.style.backgroundPosition = "0px " + scrollPercent + "%";
-    });
+    };
 })();
