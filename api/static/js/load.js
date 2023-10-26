22a28df6a0f92e724f398ba97357b3ae7c0607dc
Inesh <79165198+somebody4545@users.noreply.github.com>
2023-10-25 17:52:19 -0700

    fixed loading bug

--- api/static/js/load.js
+++ api/static/js/load.js
@@ -8,12 +8,9 @@ const setVisible = (elementOrSelector, visible) =>
   ).style.display = visible ? 'block' : 'none';
 
 page = document.getElementById("page")
-setVisible('.page', false);
-setVisible('#loading', true);
 
 document.addEventListener('DOMContentLoaded', () =>
   wait(3000).then(() => {
-    setVisible('.page', true);
     requestAnimationFrame(function() {
         page.classList.add("visible");
       });
