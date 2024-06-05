58f8916493d87f483621a93f301882dc3de6ebdc
Inesh <79165198+somebody4545@users.noreply.github.com>
2024-06-05 15:54:44 -0700

    sitemapper

--- api/main.py
+++ api/main.py
@@ -1,18 +1,24 @@
 import os
 
 from flask import Flask, render_template
-
+from flask_sitemapper import Sitemapper
 app = Flask(__name__)
 ASSETS_DIR = os.path.dirname(os.path.abspath(__file__))
+sitemapper = Sitemapper()
+sitemapper.init_app(app)
 
-
+@sitemapper.include(lastmod="2024-06-5")
 @app.route('/')
 def index():
     return render_template('index.html')
-
+@sitemapper.include(lastmod="2024-06-5")
 @app.route('/walls')
 def walls():
     return render_template('walls.html')
+
+@app.route("/sitemap.xml")
+def sitemap():
+  return sitemapper.generate()
 
 context = ('server.crt', 'server.key')
 if __name__ == "__main__":
