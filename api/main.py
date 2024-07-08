30708d14188afb43386efba8f9d1f6be875c798b
Inesh <79165198+somebody4545@users.noreply.github.com>
2024-07-07 20:01:23 -0700

    added a redirect

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_sitemapper import Sitemapper
 app = Flask(__name__)
 ASSETS_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -15,6 +15,11 @@ def index():
 @app.route('/walls')
 def walls():
     return render_template('walls.html')
+
+@sitemapper.include(lastmod="2024-06-5")
+@app.route('/wd2024')
+def wd2024():
+    return redirect("https://forms.gle/387UorEh44HUrNj17", code="301")
 
 @app.route("/sitemap.xml")
 def sitemap():
